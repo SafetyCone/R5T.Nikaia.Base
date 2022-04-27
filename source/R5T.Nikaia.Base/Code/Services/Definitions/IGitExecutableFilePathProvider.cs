b39ac9b673644297d59ab1a25775a0cf3af1fb36
davidcoats <david@getsafetycone.com>
2022-04-27 15:33:08 -0700

    Interim changes.

--- source/R5T.Nikaia.Base/Code/Services/Definitions/IGitExecutableFilePathProvider.cs
+++ source/R5T.Nikaia.Base/Code/Services/Definitions/IGitExecutableFilePathProvider.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Nikaia
-{
-    public interface IGitExecutableFilePathProvider
+{[ServiceDefinitionMarker]
+    public interface IGitExecutableFilePathProvider:IServiceDefinition
     {
         string GetGitExecutableFilePath();
     }
